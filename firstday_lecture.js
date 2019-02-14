5a069257c33cb002d927eee488abc74022323b38
Sarah Proctor <sproctor950@gmail.com>
2019-02-14 10:51:44 -0800

    added intro notes from the first day

---
+++ firstday_lecture.js
@@ -0,0 +1,37 @@
+//JavaScript Intro
+
+//Data Types
+
+// Numbers – integers, floats
+// 3, 5.9, -7, 127, 73.9, 0
+// Strings – anything stored in quotation marks
+// 	“Hello!”    “Today’s date is 1/9/2019.”     “6 + 4”
+// Boolean – true or false
+// 	true/false are not strings, they equate to the value of true or false
+// Null – equal to nothing
+// Undefined – a variable that has not been assigned a value
+// Symbol – unique and immutable identifier
+
+console.log(5 ** 6);
+
+// Variables
+// A placeholder for larger amounts of information
+// Variables need a declaration and an assignment
+// Use the keyword var to declare a variable and = to assign
+// Can be named almost anything, with the exception of reserved words
+
+var userName = "LearnStudent"
+console.log(userName);
+
+var favNum = 7
+console.log(favNum/2);
+
+var name = "Learn"
+console.log(name.length);
+
+var newNum = favNum*26
+
+console.log("fave:", favNum, "new:", newNum);
+
+console.log(newNum.includes("a"));
+//.includes is a string method, it won't work on a number
